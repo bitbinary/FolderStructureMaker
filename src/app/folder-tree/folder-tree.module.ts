00d34ab22f62ff4c571d3aabe718cfd78bb5a752
ameljohny <ameljohny.mail@gmail.com>
2021-10-17 16:04:03 +1100

    added page skeleton, modified folder structure

--- src/app/folder-tree/folder-tree.module.ts
+++ src/app/folder-tree/folder-tree.module.ts
@@ -2,12 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FolderTreeRoutingModule } from './folder-tree-routing.module';
-import { FolderTreeComponent } from './folder-tree/folder-tree.component';
+//Components
+import { FolderTreeComponent } from './folder-tree.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
-
+import { PageContentComponent } from './components/page-content/page-content.component';
+//External components
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { TreeNodeComponent } from './components/tree-node/tree-node.component';
+import { PageFooterComponent } from './components/page-footer/page-footer.component';
 @NgModule({
-  declarations: [FolderTreeComponent, PageHeaderComponent],
-  imports: [CommonModule, FolderTreeRoutingModule],
+  declarations: [
+    FolderTreeComponent,
+    PageHeaderComponent,
+    PageContentComponent,
+    TreeNodeComponent,
+    PageFooterComponent,
+  ],
+  imports: [
+    CommonModule,
+    FolderTreeRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   exports: [FolderTreeComponent],
 })
 export class FolderTreeModule {}
